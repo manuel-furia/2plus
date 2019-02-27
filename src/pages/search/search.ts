28aabd15717f97eb8b32b962c138d968cbf1736f
YouqinS <33859677+YouqinS@users.noreply.github.com>
2019-02-28 00:35:33 +0200

    search with title input by user

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs';
+import { Media } from '../../interfaces/media';
+import { MediaProvider } from '../../providers/media/media';
+import { SingleItemPage } from '../single-item/single-item';
 
 @IonicPage()
 @Component({
@@ -10,20 +14,42 @@ export class SearchPage {
   publishDate: Date;
   location: string;
   priceOrder: any;
-  searchTerm: string;
   @ViewChild('searchForm') searchForm: any;
-  showSearchForm: true;
+  showSearchForm:boolean = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public mediaProvider:MediaProvider) {
   }
+
+  public uploadUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  public mediaArray: Observable<Media[]>;
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
   }
+  searchTerm;
 
-  search() {
+//search with title only, without filter terms yet
+  search(){
+    console.log('search term: ', this.searchTerm);
+    let data = {"title": this.searchTerm};
+    console.log('search term: ', data);
 
+    this.mediaArray = this.mediaProvider.search(data);
+      this.showSearchForm = false;
+
+    console.log('search mediaArray: ', this.mediaArray);
   }
+
+
+
+  viewSingleMedia(file_id: number) {
+    console.log('view single search item: ', file_id);
+    this.navCtrl.push(SingleItemPage,{
+      file_id: file_id
+    });
+    this.showSearchForm = false;
+  }
+
 
   cancelSearch() {
     this.searchForm.reset();
