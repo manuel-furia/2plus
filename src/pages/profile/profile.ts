a9821d25e2480e350fa6c46fe48b30d78ebe3c41
YouqinS <33859677+YouqinS@users.noreply.github.com>
2019-02-28 23:39:30 +0200

    set/update profile image

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -3,6 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MediaProvider } from '../../providers/media/media';
 import { UserAuthenticationProvider } from '../../providers/user-authentication/user-authentication';
 import { MyItemsPage } from '../my-items/my-items';
+import { UploadPage } from '../upload/upload';
+import { SingleItemPage } from '../single-item/single-item';
+import { Media } from '../../interfaces/media';
 
 
 @IonicPage()
@@ -19,11 +22,42 @@ export class ProfilePage {
   }
 
   public filePath = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  avatarID;
 
+  ngOnInit(){
+    this.getAvatar();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+   // this.getAvatar();
   }
+
+  getAvatar(){
+    console.log('get avatar...');
+    this.mediaProvider.getMediaByTag('profile').subscribe(files => {
+      console.log('files with tag profile', files);
+      for(let i=0; i<files.length; i++){
+        console.log('profile img user_id: ', files[i].user_id);
+        if(files[i].user_id != this.userAuth.user.user_id)
+        delete files[i];
+      }
+      console.log('this user_id: ', this.userAuth.user.user_id);
+
+      console.log('my profile imgs?: ', files);
+      if(files.length==0) this.avatarID=null;
+     this.avatarID = files[files.length-1].file_id;
+     console.log('avatarID: ', this.avatarID);
+    })
+  }
+
+  setAvatar() {
+    this.navCtrl.push(UploadPage,{
+      tag: 'profile'
+    });
+    console.log('set avatar :)');
+  }
+
 
   logout() {
     localStorage.clear();
@@ -40,6 +74,6 @@ export class ProfilePage {
       this.mediaProvider.presentToast(res.message);
       localStorage.clear();
     })
-
   }
+
 }
