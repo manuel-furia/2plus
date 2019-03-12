7d698eaca4e09737dacde8718fc33ed0f16d4ff9
Manuel Furia <31704915+manuel-furia@users.noreply.github.com>
2019-03-12 15:17:25 +0200

    JS-015: Add storage provider

---
+++ src/providers/storage/storage.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { LoginResponse } from '../../interfaces/loginResponse';
+import { StoredSession } from "../../interfaces/storedSession";
+
+@Injectable()
+export class StorageProvider {
+  user:User;
+
+  constructor() {
+  }
+
+  public deleteSession(){
+    localStorage.removeItem('session')
+  }
+
+  public storeSession(token: string, user: User) {
+    const storedSession: StoredSession = {token: token, user: user};
+    localStorage.setItem('session', JSON.stringify(storedSession));
+  }
+
+  public loadSessionToken(): string | null {
+    const storedSession: StoredSession = JSON.parse(localStorage.getItem('session') || '{}');
+    if ('token' in storedSession)
+      return storedSession.token;
+    else
+      return null;
+  }
+
+  public loadSessionUser(): User | null {
+    const storedSession: StoredSession = JSON.parse(localStorage.getItem('session') || '{}');
+    if ('user' in storedSession)
+      return storedSession.user;
+    else
+      return null;
+  }
+
+
+}
